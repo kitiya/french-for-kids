5e006ec852b775789501a63eff972d5eb4125892
Kitiya <kate.kitiya@gmail.com>
2020-03-24 04:40:56 -0400

    Updates

---
+++ precache-manifest.c4f6f0abbc74e6cf7973610054788f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2785624a37d2319b75ed7ee7cd376e",
+    "url": "/french-for-kids/index.html"
+  },
+  {
+    "revision": "2ec3fb0fcf606de24284",
+    "url": "/french-for-kids/static/css/main.685dea8f.chunk.css"
+  },
+  {
+    "revision": "6847fc92800347ea4878",
+    "url": "/french-for-kids/static/js/2.6f1bb2f8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/french-for-kids/static/js/2.6f1bb2f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec3fb0fcf606de24284",
+    "url": "/french-for-kids/static/js/main.9d91d40d.chunk.js"
+  },
+  {
+    "revision": "3fbc0d6e827c3d5a1e12",
+    "url": "/french-for-kids/static/js/runtime-main.aef90760.js"
+  }
+]);
